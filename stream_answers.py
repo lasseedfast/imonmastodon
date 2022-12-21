baeecfef24c0d96c4ebe4ab45e44678ac85ec6ea
Lasse <lasse.edfast@gmail.com>
2022-12-21 18:59:39 +0000

    Print exception.

--- stream_answers.py
+++ stream_answers.py
@@ -38,12 +38,16 @@ def stream():
                     print(mastodon_username)
                     try:
                         source_tweet = str(json_response['data']['id'])
-                    except:
+                    except Exception as e:
+                        print(json_response)
+                        print(e)
                         source_tweet = 'user'
                     # Add Mastodon username to db.
                     db.insert_user(twitter_username, mastodon_username, source_tweet)
 
-            except:
+            except Exception as e:
+                print(json_response)
+                print(e)
                 pass
 
 
